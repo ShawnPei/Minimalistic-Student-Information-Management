ebc43f4645d5a321e9733803d383d6d4c0f2be0c
peixy <shawnpei0313@gmail.com>
2023-12-03 16:58:41 -0500

    Using Spring initilizr to build the project

---
+++ src/main/java/com/shawnpei/fullstackmanagement/FullStackManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shawnpei.fullstackmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackManagementApplication.class, args);
+    }
+
+}
