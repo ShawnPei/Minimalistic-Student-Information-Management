4eb3e0fef9a9d2142c28be30094224b6f3ce5506
peixy <shawnpei0313@gmail.com>
2023-12-03 18:49:48 -0500

    Using http-proxy-middleware to address CORS problem, using React useState and useEffect to render the data fetched from backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {getAllStudents} from "./client"; //for the getAllStudents function
+import {useEffect, useState} from 'react'; //prevent to show 2 times the same message
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // This line is using the useState hook to create a state variable students
+    // and a function setStudents to update that state.
+    // The initial value of students is an empty array []
+    const [students, setStudents] = useState([]);
+    //This is a function that fetches student data from an API.
+    // It uses the getAllStudents function to get the data. Once the data is received,
+
+    const fetchStudents = () => {
+        getAllStudents()
+            // it is converted to JSON
+            .then(response => response.json())
+            .then(data => {
+                    //and then logged to the console.
+                    console.log(data);
+                    // The data is also set to the students state using the setStudents function.
+                    setStudents(data)
+                }
+            );
+    }
+    //The function passed to useEffect will run after the component's first render,
+    // and every time the component re-renders if any values in the dependency array (the second argument to useEffect) change.
+    //In this case, the dependency array is empty, so the array will never change, so it will only render once.
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, []); //prevent to show 2 times the same message
+
+    return <p> {JSON.stringify(students)} </p>;
 }
 
 export default App;
