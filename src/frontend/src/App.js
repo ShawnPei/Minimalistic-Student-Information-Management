edcfe34d66e9d782105eb997a55cffcf04fbe102
peixy <shawnpei0313@gmail.com>
2023-12-03 19:18:09 -0500

    Using AntD Layout component and table to better render data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,71 @@
 import './App.css';
 import {getAllStudents} from "./client"; //for the getAllStudents function
 import {useEffect, useState} from 'react'; //prevent to show 2 times the same message
+
+//dependencies for the Layout
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+import {Breadcrumb, Layout, Menu, Table, theme} from 'antd';
+//Variables for the Layout
+const {Header, Content, Footer, Sider} = Layout;
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    // getItem('Option 1', '1', <PieChartOutlined />),
+    // getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined/>, [
+        getItem('Admin', '3'),
+    ]),
+    // getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    // getItem('Files', '9', <FileOutlined/>),
+];
+
+//for displaying the data
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    }
+];
+
 function App() {
+    //This line is using the useState hook to create a state variable collapsed
+    //The inital state is false, not collapse
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: {colorBgContainer},
+    } = theme.useToken();
+
     // This line is using the useState hook to create a state variable students
     // and a function setStudents to update that state.
     // The initial value of students is an empty array []
@@ -29,7 +93,57 @@ function App() {
         fetchStudents();
     }, []); //prevent to show 2 times the same message
 
-    return <p> {JSON.stringify(students)} </p>;
+    //for rendering the student
+    const renderStudents = () => {
+        if (students.length <= 0) {
+            return <div>No students!</div>
+        }
+        return <Table dataSource={students} columns={columns}/>
+    }
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="demo-logo-vertical"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        {/*<Breadcrumb.Item>User</Breadcrumb.Item>*/}
+                        {/*<Breadcrumb.Item>Bill</Breadcrumb.Item>*/}
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        {renderStudents()}
+                    </div>
+                </Content>
+
+            </Layout>
+        </Layout>
+    )
 }
 
 export default App;
